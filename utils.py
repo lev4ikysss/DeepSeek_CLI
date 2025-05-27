b4dae02ef188381496425025f6334c86f12fd7ad
Lev Palchik <levpalcik@gmail.com>
2025-05-27 08:48:52 +0300

    classes update

--- utils.py
+++ utils.py
@@ -4,10 +4,14 @@ import json
 class AI :
     def __init__(self, token: str, url: str, model: str):
         self.token = token
-        self.url = url
+        self.url = url + "chat/completions" if url[-1] == "/" else url + "/chat/completions"
         self.model = model
 
-    def send_response(self, history: list) :
+    def send_response(self, history: list, ask: str) :
+        history.append({
+            'role': 'user',
+            'content': ask
+        })
         response = requests.post(
             url=self.url,
             headers={
@@ -24,3 +28,20 @@ class AI :
             'content': response['choices'][0]['message']['content']
         })
         return history
+    
+class Files :
+    def get_chats():
+        with open('data.json', 'r') as file :
+            return json.load(file)['chats']
+    
+    def get_history(name_chat: str) :
+        with open(f'/chats/{name_chat}.json', 'r') as file :
+            return json.load(file)
+        
+    def load_history(name_chat: str, history: dict) :
+        with open(f'/chats/{name_chat}.json', 'w') as file :
+            return json.dump(history, file, indent=4)
+        
+    def new_chat(name_chat: str) :
+        with open(f'/chats/{name_chat}.json', 'w') as file :
+            return json.dump({}, file, indent=4)
