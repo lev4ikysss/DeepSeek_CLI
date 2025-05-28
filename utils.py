358eca5aa3cbe6af78a3e6bedabc871205129d65
Lev Palchik <levpalcik@gmail.com>
2025-05-28 20:10:30 +0300

    ai-shell

--- utils.py
+++ utils.py
@@ -30,36 +30,38 @@ class AI :
         return history
 
 class Files :
-    def __init__(self, path: str) :
-        self.path = path
-
-    def get_chats(self) -> list :
-        with open(f'{self.path}/data.json', 'r') as file :
+    def get_chats() -> list :
+        with open('/usr/share/ai-shell/data.json', 'r') as file :
             return json.load(file)['chats']
 
-    def get_history(self, name_chat: str) -> dict :
-        with open(f'{self.path}/data.json', 'r') as file :
+    def get_history(name_chat: str) -> dict :
+        with open('/usr/share/ai-shell/data.json', 'r') as file :
             chats = json.load(file)['chats']
         if not name_chat in chats :
             return {'error': 1}
-        with open(f'{self.path}/chats/{name_chat}.json', 'r') as file :
+        with open(f'/usr/share/ai-shell/chats/{name_chat}.json', 'r') as file :
             return json.load(file)
 
-    def load_history(self, name_chat: str, history: dict) -> None :
-        with open(f'{self.path}/chats/{name_chat}.json', 'w') as file :
+    def load_history(name_chat: str, history: dict) -> None :
+        with open(f'/usr/share/ai-shell/chats/{name_chat}.json', 'w') as file :
             return json.dump(history, file, indent=4)
 
-    def new_chat(self, name_chat: str) -> None :
-        with open(f'{self.path}/data.json', 'r') as file :
+    def new_chat(name_chat: str) -> None :
+        with open('/usr/share/ai-shell/data.json', 'r') as file :
             data = json.load(data)
         data['chats'].append(name_chat)
-        with open(f'{self.path}/chats/{name_chat}', 'w') as file :
+        with open(f'/usr/share/ai-shell/chats/{name_chat}', 'w') as file :
             json.dump({}, file, indent=4)
 
 class Work :
-    def __init__(self, url: str, model: str, token: str, path: str) :
+    def __init__(self, url: str, model: str, token: str) :
         self.AI = AI(token, url, model)
-        self.Files = Files(path)
-        with open(f'{path}/data.json', 'r') as file :
+        self.Files = Files()
+        with open('/usr/share/ai-shell/data.json', 'r') as file :
             self.chat = json.load(file)['active_chat']
 
+    def new_message(self, message: str) :
+        history = self.Files.get_history(self.chat)
+        history = self.AI.send_response(history, message)
+        self.Files.load_history(self.chat, history)
+        return history[-1]['content']
