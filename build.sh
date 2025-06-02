d33aa67594e3f4fae22dfef5c4ebfcdc57de2eb5
Lev Palchik <levpalcik@gmail.com>
2025-06-02 20:50:43 +0300

    Alpha 1

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
-mkdir -p /usr/share/ai-shell/chats
-cp -r * /usr/share/ai-shell
-python -m venv /usr/share/ai-shell/.venv
-/usr/share/ai-shell/.venv/bin/pip install -r req.txt
-cp main.py /usr/bin/ai
-cp main.py /usr/bin/ai-shell
+#!/bin/bash
+
+sudo mkdir -pv /usr/share/ai-shell/
+sudo cp -rv * /usr/share/ai-shell/
+sudo python -m venv /usr/share/ai-shell/.venv
+sudo /usr/share/ai-shell/.venv/bin/pip install -r req.txt
+sudo cp -v main.py /usr/bin/ai
+sudo cp -v main.py /usr/bin/ai-shell
+mkdir -pv ~/.local/share/ai-shell/chats/
+cp -v data.json ~/.local/share/ai-shell/
+cp -v chats/base_chat.json ~/.local/share/ai-shell/chats/
+sudo chmod -v 777 /usr/bin/ai
+sudo chmod -v 777 /usr/bin/ai-shell
+
+echo "Success! Please run \"ai config\" for change setting"
--- main.py
+++ main.py
@@ -1,15 +1,110 @@
 #!/usr/share/ai-shell/.venv/bin/python3
+import json
+import os
+import requests
 import click
-import utils
 
-Functon = utils.Work()
+path = os.path.expanduser('~/.local/share/ai-shell/')
+
+class AI :
+    def __init__(self, token: str, url: str, model: str) :
+        self.token = token
+        self.url = url + "chat/completions" if url[-1] == "/" else url + "/chat/completions"
+        self.model = model
+
+    def send_response(self, history: list, ask: str) :
+        history.append({
+            'role': 'user',
+            'content': ask
+        })
+        response = requests.post(
+            url=self.url,
+            headers={
+                "Authorization": f"Bearer {self.token}",
+                "Content-Type": "application/json"
+            },
+            data=json.dumps({
+                "model": self.model,
+                "messages": history
+            })
+        ).json()
+        history.append({
+            'role': 'assistant',
+            'content': response['choices'][0]['message']['content']
+        })
+        return history
+
+class Files :
+    @staticmethod
+    def get_chats() -> list :
+        with open(f'{path}data.json', 'r') as file :
+            return json.load(file)['chats']
+
+    @staticmethod
+    def get_history(name_chat: str) -> dict :
+        with open(f'{path}data.json', 'r') as file :
+            chats = json.load(file)['chats']
+        if not name_chat in chats :
+            return {'error': 1}
+        with open(f'{path}chats/{name_chat}.json', 'r') as file :
+            return json.load(file)
+
+    @staticmethod
+    def load_history(name_chat: str, history: dict) -> None :
+        with open(f'{path}chats/{name_chat}.json', 'w') as file :
+            return json.dump(history, file, indent=4)
+
+    @staticmethod
+    def new_chat(name_chat: str) -> None :
+        with open(f'{path}data.json', 'r') as file :
+            data = json.load(file)
+        data['chats'].append(name_chat)
+        with open(f'{path}data.json', 'w') as file :
+            json.dump(data, file, indent=4)
+        with open(f'{path}chats/{name_chat}.json', 'w') as file :
+            json.dump([], file, indent=4)
+
+    @staticmethod
+    def change_data(url: str, model: str, token: str) -> None :
+        with open(f'{path}data.json', 'r') as file :
+            data = json.load(file)
+        data['url'] = url
+        data['model'] = model
+        data['token'] = token
+        with open(f'{path}data.json', 'w') as file :
+            json.dump(data, file, indent=4)
+
+class Work :
+    def __init__(self) :
+        with open(f'{path}data.json', 'r') as file :
+            data = json.load(file)
+            self.chat = data['active_chat']
+            self.AI = AI(data['token'], data['url'], data['model'])
+        self.Files = Files()
+
+    def new_message(self, message: str) -> str :
+        history = self.Files.get_history(self.chat)
+        history = self.AI.send_response(history, message)
+        self.Files.load_history(self.chat, history)
+        return history[-1]['content']
+    
+    def set_active_chat(self, name_chat: str) -> int :
+        with open(f'{path}data.json', 'r') as file :
+            data = json.load(file)
+        if not name_chat in data['chats'] :
+            return 1
+        data['active_chat'] = name_chat
+        with open(f'{path}data.json', 'w') as file :
+            json.dump(data, file, indent=4)
+
+Functon = Work()
 
 @click.group()
 def main() :
     pass
 
 @main.command()
-@click.argument("msg", type=str, help="Message for response")
+@click.argument("msg")
 def ask(msg) :
     answr = Functon.new_message(msg)
     print(f'ai:\n{answr}')
@@ -61,7 +156,10 @@ def see_history(name) :
 @main.command()
 def see_chats() :
     chats = Functon.Files.get_chats()
-    print(f"Chats: {*chats}")
+    print("Chats: ", end="")
+    for chat in chats :
+        print(chat, end=" ")
+    print("\n")
 
 if __name__ == "__main__" :
     main()
